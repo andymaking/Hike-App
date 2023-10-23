72fc7ee72f82360cff860b8f369b2098e864ff99
Andima Udoh <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>
2023-10-23 06:00:07 +0100

    Initial Commit

---
+++ Hike App/Hike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_AppApp.swift
+//  Hike App
+//
+//  Created by Andima Udoh on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Hike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
